9f9bf741b72d6239ddfd53c1af06d97fed990114
Chase Fite <chasef7031@gmail.com>
2020-01-27 14:43:41 -0600

    new master

--- actions/report.py
+++ actions/report.py
@@ -1,5 +1,17 @@
 def build_facility_report(arboretum):
-    for river in arboretum.rivers:
-        print(f'River [{river.id}]')
+    display_habitat_info(arboretum.mountains)
+    display_habitat_info(arboretum.grasslands)
+    display_habitat_info(arboretum.rivers)
+    display_habitat_info(arboretum.forests)
+    display_habitat_info(arboretum.swamps)
+    display_habitat_info(arboretum.coastlines)
+
+# takes a list of habitats and displays each habitat and the animals contained in it
+def display_habitat_info(habitat_list):
+    for habitat in habitat_list:
+        print(f'{habitat.name} [{str(habitat.id)[0:8]}]')
+        # need the name of the animal list variable
+        for animal in habitat.animal_list:
+            print(f'{animal.name} ({str(animal.id)[0:8]})')
 
     input("\n\nPress any key to continue...")
