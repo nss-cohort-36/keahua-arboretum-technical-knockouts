207a6875b5ee9a10363406019eb6bfb88c01a27a
Chase Fite <chasef7031@gmail.com>
2020-01-29 09:29:21 -0600

    new master

--- interfaces/habitat/contains_animals.py
+++ interfaces/habitat/contains_animals.py
@@ -1,4 +1,19 @@
 class IContainsAnimals():
 
     def __init__(self):
-        self.animals = []
+        self.__animals = []
+        self.__animal_count = 0
+        
+    @property
+    def animals(self):
+        return self.__animals
+
+    def add_animal(self, animal):
+        self.__animals.append(animal)
+
+    @property
+    def animal_count(self):
+        return self.__animal_count
+
+    def increment_animal_count(self):
+        self.__animal_count += 1
